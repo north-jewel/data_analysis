bd40fc13e1595610265be65875c2acc2ebaf1ea8
Qslip <40734793+Qslip@users.noreply.github.com>
2018-12-15 21:09:37 +0800

    Add files via upload
    优化

--- homework/吴博伟-Qslip/PCauto_thread.py
+++ homework/吴博伟-Qslip/PCauto_thread.py
@@ -2,9 +2,10 @@
 # @Time: 2018/12/14 16:16
 # @File: PCauto_thread.py
 import requests
-import numpy as np
 from bs4 import BeautifulSoup
 import pandas as pd
+import threading
+import time
 
 
 class PCT:
@@ -13,29 +14,31 @@ class PCT:
     home_url = 'https://price.pcauto.com.cn'
 
     def __init__(self):
-        self.type_page = np.delete(np.arange(20), 12)
+        pass
 
     def get_url(self, arg):
         res = requests.get(arg)
         res.encoding = 'gb2312'
         doc = res.text
-        soup = BeautifulSoup(doc, 'html.parser')
-        return soup
+        return doc
 
-    def page_dispose(self, soup):
-        for i in self.type_page:
-            soup = self.get_url(self.type_url.format(i))
-            pc_page = soup.find('div', class_='pcauto_page').find_all('a')[-1].text
-            if pc_page == '下一页':
-                # print('第{}种车型 第{}页'.format(i))
-                max_page = int(soup.find('div', class_='pcauto_page').find_all('a')[-2].text)
-                for x in range(1, max_page + 1):
-                    if x == 1:
-                        self.data_set(soup)
-                    else:
-                        self.data_set(self.get_url(self.type_page_url.format(i, x)))
-            else:
-                self.data_set(soup)
+    def page_dispose(self, page):
+        soup = BeautifulSoup(self.get_url(self.type_url.format(page)), 'html.parser')
+        try:
+            max_page = int(soup.find_all('div', class_='pcauto_page')[0].find_all('a')[-2].text)
+            print('第{}种车型 总{}页'.format(page, max_page))
+        except IndexError:
+            print('第{}种车型仅有一页'.format(page))
+            return self.data_set(soup)
+        else:
+            for x in range(3, max_page + 1):
+                if x == 1:
+                    self.data_set(soup)
+                else:
+                    print('第{}种车型 第{}页 总{}页'.format(page, x,max_page))
+                    doc = self.get_url(self.type_page_url.format(page, x))
+                    soup = BeautifulSoup(doc, 'html.parser')
+                    self.data_set(soup)
 
     def data_set(self, soup):
         data = []
@@ -62,13 +65,14 @@ class PCT:
             if T == 4:
                 MIIT_oil_list.append(all_list[i].text.replace('工信部油耗：', ''))
             if T == 5:
-                cc_list.append(all_list[i].text.replace('级别：', ''))
+                rank_list.append(all_list[i].text.replace('级别：', ''))
             if T == 6:
-                o = ''
+                o = []
                 b = [j for j in all_list[i].find_all('a')]
                 for t in range(len(b)):
-                    o = o + b[t].text + '/'
-                rank_list.append(o)
+                    o.append(b[t].text)
+                cc_list.append(o)
+                T = 0
         data.append(g_price_list)
         data.append(owner_oil_list)
         data.append(brand_list)
@@ -78,89 +82,118 @@ class PCT:
         #  dealer_price_list = []  # 经销商报价
         all_arg_list = []  #
         for i in detail_url_list:  #
-            arg_list = []  #
-            url = self.home_url + i
-            doc = requests.get(url)
-            doc.encoding = 'gb2312'
-            doc = doc.text
+            arg_list = []
+            time.sleep(0.2)
+            doc = self.get_url(self.home_url + i)
             soup = BeautifulSoup(doc, 'html.parser')
-            gearbox = ''  # 变速箱
-            for i in range(1, 3):
-                if len(gearbox) > 1:
-                    gearbox = gearbox + soup.select('#bsx_{}'.format(i))[0].text  # 变速箱
-                else:
-                    gearbox = gearbox + soup.select('#bsx_{}'.format(i))[0].text + '|'  # 变速箱
+            print(self.home_url + i)
+            gearbox = []  # 变速箱
+            for i in range(6):
+                try:
+                    gearbox.append(soup.select('#bsx_{}'.format(i))[0].text)  # 变速箱
+                except IndexError:
+                    pass
             arg_list.append(gearbox)
-            cartype = soup.find('ul', class_='des').find_all('a')[-1].text  # 车身类型
+            cartype = []  # 车身类型
+            for x in soup.find('ul', class_='des').find_all('a'):
+                if x.text[-1] == '车':
+                    cartype.append(x.text)
+            if len(cartype) >= 2:
+                cartype = cartype[-1]
+                if type(cartype) != list:
+                    cartype = [cartype]
+            else:
+                pass
             arg_list.append(cartype)
-            ranking = soup.select('.strength > .tip ')[0].text.replace('\n', '').replace('>', '')  # 排名
-            arg_list.append(ranking)
+            ranking = soup.select('.strength > .tip ')[0].text.replace('\n', '').replace('>', '').replace('\r', '')  # 排名
+            arg_list.append([ranking])
+
             all_message = soup.select('.processBar-txt')
             for i in range(len(all_message)):
                 if i == 0:
-                    carstrength = all_message[i].em.text + all_message[i].p.text  # 车实力
-                    arg_list.append(carstrength)
+                    try:
+                        carstrength = all_message[i].em.text + all_message[i].p.text  # 车实力
+                    except AttributeError:
+                        carstrength = all_message[i].p.text  # 车实力
+                    arg_list.append([carstrength])
                 if i == 1:
                     configuration = all_message[i].p.text  # 配置水平
-                    arg_list.append(configuration)
+                    arg_list.append([configuration])
                 if i == 2:
                     major = all_message[i].p.text  # 专业测评
-                    arg_list.append(major)
+                    arg_list.append([major])
                 if i == 3:
                     ownerevaluate = all_message[i].p.text  # 车主评价
-                    arg_list.append(ownerevaluate)
+                    arg_list.append([ownerevaluate])
                 if i == 4:
                     price = all_message[i].p.text  # 价格
-                    arg_list.append(price)
+                    arg_list.append([price])
                 if i == 5:
-                    overall = all_message[i].p.text  # 综合评分
-                    arg_list.append(overall)
-            all_carlye = soup.find('div', class_='selCar').find_all('em')
-            all_caprice = soup.find('div', class_='selCar').find_all('i')
-            if len(all_carlye) >= 2:
-                versions1 = all_carlye[0].text
-                versionsp1 = all_caprice[0].text
-                versions2 = all_carlye[1].text
-                versionsp2 = all_caprice[1].text
-                arg_list.append(versions1)
-                arg_list.append(versionsp1)
-                arg_list.append(versions2)
-                arg_list.append(versionsp2)
-                all_arg_list.append(arg_list)
-                continue
-            if len(all_carlye) <= 1:
-                versions1 = all_carlye[0].text
-                versionsp1 = all_caprice[0].text
-                versions2 = ''
-                versionsp2 = ''
-                arg_list.append(versions1)
-                arg_list.append(versionsp1)
-                arg_list.append(versions2)
-                arg_list.append(versionsp2)
-                all_arg_list.append(arg_list)
-                continue
-            else:
+                    # overall = all_message[i].p.text  # 综合评分
+                    # arg_list.append([overall])
+                    pass
+            if len(arg_list) < 5:
+                for i in range(5-len(arg_list)):
+                    arg_list.append(None)
+
+            try:
+                all_carlye = soup.find('div', class_='selCar').find_all('em')
+                all_caprice = soup.find('div', class_='selCar').find_all('i')
+            except AttributeError:
                 versions1 = ''
                 versionsp1 = ''
                 versions2 = ''
                 versionsp2 = ''
-                arg_list.append(versions1)
-                arg_list.append(versionsp1)
-                arg_list.append(versions2)
-                arg_list.append(versionsp2)
+                arg_list.append([versions1])
+                arg_list.append([versionsp1])
+                arg_list.append([versions2])
+                arg_list.append([versionsp2])
                 all_arg_list.append(arg_list)
-            data.append(all_arg_list)
-        return data
+            else:
+                if len(all_carlye) >= 2:
+                    versions1 = all_carlye[0].text
+                    versionsp1 = all_caprice[0].text
+                    versions2 = all_carlye[1].text
+                    versionsp2 = all_caprice[1].text
+                    arg_list.append([versions1])
+                    arg_list.append([versionsp1])
+                    arg_list.append([versions2])
+                    arg_list.append([versionsp2])
+                    all_arg_list.append(arg_list)
+                    continue
+                if len(all_carlye) <= 1:
+                    versions1 = all_carlye[0].text
+                    versionsp1 = all_caprice[0].text
+                    versions2 = ''
+                    versionsp2 = ''
+                    arg_list.append([versions1])
+                    arg_list.append([versionsp1])
+                    arg_list.append([versions2])
+                    arg_list.append([versionsp2])
+                    all_arg_list.append(arg_list)
+                    continue
+                else:
+                    versions1 = ''
+                    versionsp1 = ''
+                    versions2 = ''
+                    versionsp2 = ''
+                    arg_list.append([versions1])
+                    arg_list.append([versionsp1])
+                    arg_list.append([versions2])
+                    arg_list.append([versionsp2])
+                    all_arg_list.append(arg_list)
+        data.append(all_arg_list)
+        self.save_csv(data)
 
     def save_csv(self, data):
         columns = ['carname', 'officialprice', 'carfuelc', 'brand', 'MIITfuelc',
-                   'displacement', 'rank', 'gearbox', 'cartype','ranking',
-                   'carstrength', 'configuration', 'major', 'ownerevaluate', 'price',
-                   'overall', 'versions1', 'versionsp1', 'versions2', 'versionsp2']
+                   'displacement', 'rank', 'gearbox', 'cartype', 'ranking',
+                   'carstrength', 'configuration', 'major', 'ownerevaluate',
+                   'price', 'versions1', 'versionsp1', 'versions2', 'versionsp2']
         data_set = []
         for i in range(len(data[0])):
             set = []
-            for x in range(len(data)):
+            for x in data:
                 set.append(x[i])
             data_set.append(set)
         data_all = []
@@ -170,4 +203,30 @@ class PCT:
                 p.append(x)
             data_all.append(p)
         df = pd.DataFrame(data_all, columns=columns)
-        df.to_csv('PCT.csv', index=False)
+        try:
+            pd.read_csv(r'C:\Users\48170\Desktop\PCT.csv', encoding='utf-8')
+            df.to_csv(r'C:\Users\48170\Desktop\PCT.csv', encoding='utf-8', index=False, mode='a', header=False)
+        except FileNotFoundError:
+            df.to_csv(r'C:\Users\48170\Desktop\PCT.csv', encoding='utf-8', index=False)
+        print('数据写入完成')
+        return True
+
+    def thread(self, td):
+        th = td
+        while True:
+            pool = []
+            for i in range((td - th) + 2, td + 2):
+                if i < 21 and i != 12:
+                    pool.append(threading.Thread(target=self.page_dispose, args=(i,)))
+            if pool:
+                for x in pool:
+                    x.start()
+                for c in pool:
+                    c.join()
+                td += th
+            else:
+                break
+        return print('所有数据爬取完成')
+
+x = PCT()
+x.thread(6)
